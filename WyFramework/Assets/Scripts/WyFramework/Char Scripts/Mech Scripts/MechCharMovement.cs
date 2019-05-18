6327b5eca8be0036547c2d7aef289feefa73f555
Wei Yuen Ng <u1274886@ad.utah.edu>
2019-05-18 17:55:31 -0600

    V0.0.1b Organized Framework Libraries

--- WyFramework/Assets/Scripts/WyFramework/Char Scripts/Mech Scripts/MechCharMovement.cs
+++ WyFramework/Assets/Scripts/WyFramework/Char Scripts/Mech Scripts/MechCharMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PlayerCharacterMovement : MonoBehaviour
+public class CharMovement : MonoBehaviour
 {
 	Rigidbody2D myRB;
 	VisCharAnim childCharAnim;
